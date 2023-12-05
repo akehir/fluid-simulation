ddf4f6ca8fb179e6decd68a8a09b1221ad6219db
Raphael Ochsenbein <raphael@akehir.com>
2023-12-05 18:15:33 +0100

    Update ci/cd

--- projects/example-app/karma.conf.js
+++ projects/example-app/karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeDocker: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--use-angle=gl', '--in-process-gpu']
+        flags: ['--no-sandbox', '--use-vulkan=native', '--use-angle=vulkan', '--in-process-gpu']
       }
     },
     singleRun: false
