22b59f5d1d9d7e72c053d703249b2d6336ce66ce
Raphael Ochsenbein <raphael@akehir.com>
2023-12-05 20:43:42 +0100

    Update ci/cd

--- projects/fluid-simulation-lib/karma.conf.js
+++ projects/fluid-simulation-lib/karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeDocker: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--use-angle=swiftshader', '--use-gl=angle', '--verbose']
+        flags: ['--no-sandbox', '--use-angle=swiftshader', '--use-gl=angle', '--in-process-gpu', '--verbose']
       }
     },
     browserConsoleLogOptions: {
